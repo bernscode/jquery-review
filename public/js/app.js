364339781ef3757672ffe5c8ac6ec00654db6ecd
bernscode <bern@bernscode.com>
2018-05-30 17:27:47 -0700

    Made first updates to understanding the syntax.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
-$('body').css({background: 'purple'})
+$('.box1').css({background: 'blue'})
+
+$('#last').css({background: 'black', padding: '20px'})
+
+$('#last p').css({background: 'red'})
+
+
+
+
+
+// END
