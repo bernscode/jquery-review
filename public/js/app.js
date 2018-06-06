aeff2edfd8f57e797bd7875cf1685f5496c78bce
bernscode <bern@bernscode.com>
2018-06-05 23:54:51 -0700

    Completed Cloning Element.

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,8 @@
 $(function(){
-  $('div').addClass('active')
-
-  if($('div').hasClass('active')) {
-    console.log('This element has been activated')
-  } else{
-    console.log('Sorry, waiting to be activated')
-  }
-
-
 
   $('.button').on('click', function(){
-    $('div').toggleClass('active')
+    $('.box1:first').clone().appendTo('body')
   })
-
-
-
 
 });
 
