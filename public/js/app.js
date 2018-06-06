c31b75fab8bc16010a1982bbb96c7c5b48e90027
bernscode <bern@bernscode.com>
2018-06-06 00:51:37 -0700

    Complete Removing An Element.

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,7 @@
 $(function(){
 
   $('.button').on('click', function(){
-    $('.firstP:first').before('<p>NEW MESSAGE</p>')
-
-    $('<p>NEW MESSAGE</p>').insertAfter('.firstP:first')
+    $('div').remove('.box')
 
   })
 
