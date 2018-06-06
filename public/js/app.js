bee99021087da85b4baa99d3e355082e28b1c2fd
bernscode <bern@bernscode.com>
2018-06-05 23:47:47 -0700

    Added toggleCLass function.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,21 @@
 $(function(){
-  $('p').addClass('active')
+  $('div').addClass('active')
+
+  if($('div').hasClass('active')) {
+    console.log('This element has been activated')
+  } else{
+    console.log('Sorry, waiting to be activated')
+  }
+
+
+
+  $('.button').on('click', function(){
+    $('div').toggleClass('active')
+  })
+
+
+
+
 });
 
 
