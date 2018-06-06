2fcde3886f69630b2a3c67a88a9563dad5f1fa8d
bernscode <bern@bernscode.com>
2018-06-05 23:36:10 -0700

    Started DOM manipulation.

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,5 @@
 $(function(){
-  $('.button1').on('click',function(){
-    console.log('bernscode')
-  })
-
-  $('.button2').hover(function(){
-    for(i = 1; i <= 25; i++) {
-      console.log(i);
-    }
-  })
-
-
-  $('.square').mouseenter(function(){
-    $(this).css({
-      background: 'red'
-    })
-  })
+  $('.button').addClass('active')
 });
 
 
