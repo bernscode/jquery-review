50a5426bcae72d49d760cd89422762a6ff4c94eb
bernscode <bern@bernscode.com>
2018-06-06 22:30:26 -0700

    Completed Laydown the HTML and CSS.

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,8 @@ $(function(){
 
   $('.btn').on('click', function(){
     var request = $.ajax({
-      url: "https://jsonplaceholder.typicode.com/posts/1",
-      method: "DELETE",
+      url: "https://pokeapi.co/api/v2/pokemon/pikachu/",
+      method: "GET",
       // data: {
       //   title: 'Top 5 Best Cities To Live',
       //   body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
