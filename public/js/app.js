609ff843de153f03fd7cc05982e7099109842b86
bernscode <bern@bernscode.com>
2018-06-06 17:01:25 -0700

    Complete AJAX POST.

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,12 @@ $(function(){
   $('.btn').on('click', function(){
     var request = $.ajax({
       url: "https://jsonplaceholder.typicode.com/posts",
-      method: "GET",
-      // data: { id : menuId },
+      method: "POST",
+      data: {
+        title: 'Top 10 Best Places To Live',
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        userId: 5
+      },
       // dataType: "html"
     });
 
