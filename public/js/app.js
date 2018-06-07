75cd5bdd4eda56e0e7d1af3e8de0f79b9953b272
bernscode <bern@bernscode.com>
2018-06-06 17:17:47 -0700

    Completed DELETE method.

--- public/js/app.js
+++ public/js/app.js
@@ -4,12 +4,12 @@ $(function(){
   $('.btn').on('click', function(){
     var request = $.ajax({
       url: "https://jsonplaceholder.typicode.com/posts/1",
-      method: "PUT",
-      data: {
-        title: 'Top 5 Best Cities To Live',
-        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        userId: 5
-      },
+      method: "DELETE",
+      // data: {
+      //   title: 'Top 5 Best Cities To Live',
+      //   body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      //   userId: 5
+      // },
       // dataType: "html"
     });
 
