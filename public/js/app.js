3c515ff895fdfb7a81ee2bda956f1732026b89cd
bernscode <bern@bernscode.com>
2018-06-07 00:44:02 -0700

    Completed Create Initial Function.

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,51 @@
 $(function(){
   var pokemonSearch;
+  var defaultPokemon = '1'
+  var defaultPokemonData;
+
+  var initFunc = function() {
+    // access all pokemon
+    // https://pokeapi.co/api/v2/pokemon/?limit=811
+    defaultPokemonData = $.ajax({
+      url: "https://pokeapi.co/api/v2/pokemon/" + defaultPokemon,
+      method: "GET",
+    });
+    defaultPokemonData.done(function( data ) {
+      defaultPokemonData = data;
+
+      $('.loading-container').removeClass('active')
+
+      $('.pokedex h3').text(defaultPokemonData.name.toUpperCase())
+
+      $('.poke-img img').attr('src', defaultPokemonData.sprites.front_default)
+      console.log(data)
+    });
+
+    defaultPokemonData.fail(function( jqXHR, textStatus, error) {
+      alert( "Request failed: " + textStatus + ' ' + error);
+    });
+  }
+
+
+  initFunc()
+
 
   $('.btn').on('click', function(){
+
+    $('.loading-container').addClass('active')
 
     pokemonSearch = $('.pokedex input[type="text"]').val()
 
     var request = $.ajax({
       url: "https://pokeapi.co/api/v2/pokemon/" + pokemonSearch,
       method: "GET",
-      // data: {
-      //   title: 'Top 5 Best Cities To Live',
-      //   body: "",
-      //   userId: 5
-      // },
-      // dataType: "html"
     });
 
     request.done(function( data ) {
 
-      $('.pokedex h3').text(data.name)
+      $('.loading-container').removeClass('active')
+
+      $('.pokedex h3').text(data.name.toUpperCase())
 
       $('.poke-img img').attr('src', data.sprites.front_default)
       console.log(data)
